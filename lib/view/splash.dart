9933d6630b39cc33462e5396ff4ef922fc6a0457
fifty <mohamedelameen50>
2021-07-14 17:26:44 +0200

    add loading to splash

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/utils/routes.dart';
+import 'package:news_app/view/customs/translatable-text.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key key}) : super(key: key);
 
   @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  Future init() async{
+    await Future.delayed(Duration(seconds: 2));
+    Navigator.of(context).pushNamed(homeRoute);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Row(
+        children: [
+          TranslatableText("Loading..."),
+          SizedBox(width: 8.0),
+          CircularProgressIndicator(),
+        ],
+      ),
+    );
   }
 }
